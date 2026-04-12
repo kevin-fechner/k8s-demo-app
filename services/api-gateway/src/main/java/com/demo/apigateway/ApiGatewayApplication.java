6a289d82dece0a85272d6f77694d84f230448f34
Kevin Fechner <kevin.fechner@outlook.com>
2026-04-12 21:33:03 +0200

    fix: remove circuit breaker filter - missing dependency

--- services/api-gateway/src/main/java/com/demo/apigateway/ApiGatewayApplication.java
+++ services/api-gateway/src/main/java/com/demo/apigateway/ApiGatewayApplication.java
@@ -3,7 +3,9 @@ package com.demo.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.cloud.autoconfigure.RefreshAutoConfiguration.class
+})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
