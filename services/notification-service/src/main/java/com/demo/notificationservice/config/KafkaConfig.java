acf9800fefc3ecbc862d843eb157f293055d8860
Kevin Fechner <kevin.fechner@outlook.com>
2026-04-21 19:32:37 +0200

    fix: add EnableKafka to KafkaConfig

--- services/notification-service/src/main/java/com/demo/notificationservice/config/KafkaConfig.java
+++ services/notification-service/src/main/java/com/demo/notificationservice/config/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 public class KafkaConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
